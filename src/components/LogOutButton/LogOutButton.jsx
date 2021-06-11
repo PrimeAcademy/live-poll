c90cdc11e479058c912a9d2af10e3adc54cb305a
Edan Schwartz <edanschwartz@gmail.com>
2021-06-11 16:28:11 -0500

    eslint fixes

--- src/components/LogOutButton/LogOutButton.jsx
+++ src/components/LogOutButton/LogOutButton.jsx
@@ -8,6 +8,7 @@ function LogOutButton(props) {
             // This button shows up in multiple locations and is styled differently
             // because it's styled differently depending on where it is used, the className
             // is passed to it from it's parents through React props
+            // eslint-disable-next-line react/prop-types
             className={props.className}
             onClick={() => dispatch({ type: 'LOGOUT' })}
         >
