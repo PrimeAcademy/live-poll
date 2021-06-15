73eefe5b82c8b53940c175d3e8b9f211fa087167
Edan Schwartz <edanschwartz@gmail.com>
2021-06-15 09:59:13 -0500

    DB Seed

--- database.sql
+++ database.sql
@@ -6,5 +6,31 @@
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    "displayName" VARCHAR (255) UNIQUE NOT NULL,
+    "password" VARCHAR (255) NOT NULL
 );
+
+CREATE TABLE "session" (
+    "id" SERIAL PRIMARY KEY,
+    "presenterId" INT REFERENCES "user",
+    "name" VARCHAR(255),
+    "joinCode" VARCHAR(12),
+    "createdAt" TIMESTAMP,
+    "endedAt" TIMESTAMP
+);
+
+CREATE TABLE "participant" (
+    "id" SERIAL PRIMARY KEY,
+    "sessionId" INT REFERENCES "session",
+    "displayName" VARCHAR(255),
+    "joinedAt" TIMESTAMP,
+    "exitedAt" TIMESTAMP
+);
+
+-- Feedback score
+CREATE TABLE "score" (
+    "id" SERIAL PRIMARY KEY,
+    "participantId" INT REFERENCES "participant",
+    "value" INT,
+    "createdAt" TIMESTAMP
+)
