78430cc20348eacdb6728fea898cc20e44b5452f
Edan Schwartz <edanschwartz@gmail.com>
2021-06-18 10:13:30 -0500

    Configure presenter + participant app on diff subdomains

---
+++ src/ParticipantApp/components/App/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <>
+            <h1>Participant App</h1>
+        </>
+    );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-
 import { CssBaseline, ThemeProvider } from '@material-ui/core';
-import store from './PresenterApp/redux/store';
-import App from './PresenterApp/components/App/App';
-import theme from './PresenterApp/Theme';
+
+import presenterStore from './PresenterApp/redux/store';
+import PresenterApp from './PresenterApp/components/App/App';
+import PresenterTheme from './PresenterApp/Theme';
+
+import ParticipantApp from './ParticipantApp/components/App/App';
+
+// Choose to run presenter vs. participant app
+// based on the subdomain
+const isPresenterSubdomain = window.location.host.startsWith(`${process.env.REACT_APP_PRESENTER_SUBDOMAIN}.`);
+const App = isPresenterSubdomain
+    ? (
+        <Provider store={presenterStore}>
+            <ThemeProvider theme={PresenterTheme}>
+                <CssBaseline />
+                <PresenterApp />
+            </ThemeProvider>
+        </Provider>
+    )
+    : (
+        <ParticipantApp />
+    );
 
 ReactDOM.render(
-    <Provider store={store}>
-        <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <App />
-        </ThemeProvider>
-    </Provider>,
+    App,
     document.getElementById('react-root'),
 );
