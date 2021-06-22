2575b54078f1b083f3961f19c19743cb51057bb4
Edan Schwartz <edanschwartz@gmail.com>
2021-06-22 16:42:44 -0500

    slick dynamic slider

--- src/ParticipantApp/components/ActiveSession/ActiveSession.jsx
+++ src/ParticipantApp/components/ActiveSession/ActiveSession.jsx
@@ -62,9 +62,10 @@ function interpolateColor(c0, c1, f) {
 
 // https://stackoverflow.com/a/13532993/830030
 function shadeColor(color, percent) {
-    let R = parseInt(color.substring(1, 3), 16);
-    let G = parseInt(color.substring(3, 5), 16);
-    let B = parseInt(color.substring(5, 7), 16);
+    let R = parseInt(color.substring(0, 2), 16);
+    let G = parseInt(color.substring(2, 4), 16);
+    let B = parseInt(color.substring(4, 6), 16);
+    console.log({ R, G, B });
 
     R = parseInt(R * (100 + percent) / 100);
     G = parseInt(G * (100 + percent) / 100);
@@ -78,7 +79,7 @@ function shadeColor(color, percent) {
     const GG = ((G.toString(16).length == 1) ? `0${G.toString(16)}` : G.toString(16));
     const BB = ((B.toString(16).length == 1) ? `0${B.toString(16)}` : B.toString(16));
 
-    return `#${RR}${GG}${BB}`;
+    return `${RR}${GG}${BB}`;
 }
 
 function ActiveSession() {
@@ -96,7 +97,13 @@ function ActiveSession() {
 
         const color = interpolateColor(lowestColor, highestColor, scoreUncommitted / 5);
         console.log({ color });
-        track.style.background = `#${color}`;
+        track.style.background = `
+            linear-gradient(
+                0deg, 
+                #${shadeColor(color, -20)} 0%,  
+                #${shadeColor(color, 20)} 78%
+            )
+        `;
     }, [scoreUncommitted]);
 
     return (
