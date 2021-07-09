e33bdedc332739267d71dba17b135d963b6935f9
Edan Schwartz <edanschwartz@gmail.com>
2021-07-09 16:24:32 -0500

    Fix join button not working, with keyboard up

--- src/ParticipantApp/components/ParticipantLogin/ParticipantLogin.jsx
+++ src/ParticipantApp/components/ParticipantLogin/ParticipantLogin.jsx
@@ -51,91 +51,91 @@ function ParticipantLogin() {
     };
 
     return (
-        <div style={{
-            padding: '50px 80px',
-            marginTop: 60,
-        }}
-        >
-            <div style={{ textAlign: 'center' }}>
-                <Logo
-                    showText={false}
-                    style={{
-                        display: 'inline-block',
-                        width: 220,
-                        marginLeft: -40,
-                    }}
-                />
-                <h2
-                    className="nav-title"
-                    style={{
-                        display: 'inline-block',
-                        fontFamily: 'Marker Felt',
-                        verticalAlign: 124,
-                        color: '#4d4d4d',
-                        textShadow: '1px 1px 4px rgba(255, 255, 255, 0.3)',
-                        fontSize: 42,
-                        padding: 0,
-                        transform: 'rotate(-8deg)',
-                        marginLeft: -90,
-                    }}
-                >
-                    LivePoll
-                </h2>
-            </div>
-
-            <h2 style={{
-                marginTop: 70,
-                marginBottom: 20,
-                fontSize: 24,
+        <>
+            <div style={{
+                padding: '10vh 80px',
             }}
             >
-                Session Code
-            </h2>
+                <div style={{ textAlign: 'center' }}>
+                    <Logo
+                        showText={false}
+                        style={{
+                            display: 'inline-block',
+                            width: 220,
+                            marginLeft: -40,
+                        }}
+                    />
+                    <h2
+                        className="nav-title"
+                        style={{
+                            display: 'inline-block',
+                            fontFamily: 'Marker Felt',
+                            verticalAlign: 124,
+                            color: '#4d4d4d',
+                            textShadow: '1px 1px 4px rgba(255, 255, 255, 0.3)',
+                            fontSize: 42,
+                            padding: 0,
+                            transform: 'rotate(-8deg)',
+                            marginLeft: -90,
+                        }}
+                    >
+                        LivePoll
+                    </h2>
+                </div>
 
-            <form onSubmit={onSubmit}>
-                {/* TODO: need a "what's a session code, where do I get one thing" */}
-                <TextField
-                    value={joinCode}
-                    onChange={(e) => setJoinCode(e.target.value.toUpperCase())}
-                    variant="outlined"
-                    style={{
-                        width: '100%',
-                    }}
-                    inputProps={{
-                        style: {
-                            padding: '8px 12px',
-                        },
-                    }}
-                />
-                <Button
-                    type="submit"
-                    variant="contained"
-                    color="primary"
-                    style={{
-                        float: 'right',
-                        marginTop: 11,
-                    }}
+                <h2 style={{
+                    marginTop: 70,
+                    marginBottom: 20,
+                    fontSize: 24,
+                }}
                 >
-                    Join!
-                </Button>
-            </form>
+                    Session Code
+                </h2>
 
+                <form onSubmit={onSubmit}>
+                    {/* TODO: need a "what's a session code, where do I get one thing" */}
+                    <TextField
+                        value={joinCode}
+                        onChange={(e) => setJoinCode(e.target.value.toUpperCase())}
+                        variant="outlined"
+                        style={{
+                            width: '100%',
+                        }}
+                        inputProps={{
+                            style: {
+                                padding: '8px 12px',
+                            },
+                        }}
+                    />
+                    <Button
+                        type="submit"
+                        variant="contained"
+                        color="primary"
+                        style={{
+                            float: 'right',
+                            marginTop: 11,
+                        }}
+                    >
+                        Join!
+                    </Button>
+                    <div style={{ clear: 'both' }} />
+                </form>
+
+            </div>
             <Link
                 to="/info"
                 style={{
                     color: 'var(--almost-black)',
                     textDecoration: 'none',
                     fontStyle: 'italic',
-                    position: 'fixed',
-                    bottom: 15,
-                    left: 0,
                     width: '100%',
+                    display: 'block',
                     textAlign: 'center',
                 }}
             >
                 What is LivePoll?
             </Link>
-        </div>
+        </>
     );
 }
 
